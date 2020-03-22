c714f59048642d57c93642098903e270063ac96f
Gene Choe <hochoe@microsoft.com>
2020-03-22 12:28:53 -0700

    Updates

---
+++ precache-manifest.572204949c8cc2cba518e0ece166be1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff12fcdb251960d10da1287517898e8",
+    "url": "/react-github-page-test/index.html"
+  },
+  {
+    "revision": "f66d51026607782f4802",
+    "url": "/react-github-page-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a9db970cb986d5474660",
+    "url": "/react-github-page-test/static/js/2.f908b780.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-github-page-test/static/js/2.f908b780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f66d51026607782f4802",
+    "url": "/react-github-page-test/static/js/main.7b4cb61e.chunk.js"
+  },
+  {
+    "revision": "022c483dc508b6c0b89d",
+    "url": "/react-github-page-test/static/js/runtime-main.15ee58c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-github-page-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
